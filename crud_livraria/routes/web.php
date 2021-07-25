998f4beedcf9db0bd987c255ae8c70c269dd7ceb
Guilherme Santos <78465227+GuilhermeSantosJS@users.noreply.github.com>
2021-07-24 21:44:02 -0300

    Adicionando serviçcos de login e cadastro de usuááario

--- crud_livraria/routes/web.php
+++ crud_livraria/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivrosController;
-
+use App\Http\Controllers\CustomAuthController;
 
 
 /*
@@ -20,10 +20,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', [CustomAuthController::class, 'dashboard']); 
+Route::get('/login', [CustomAuthController::class, 'index']) -> name('login');
+Route::post('/custom-login', [CustomAuthController::class, 'customLogin']) -> name('login.custom'); 
+Route::get('/registration', [CustomAuthController::class, 'registration']) -> name('register-user');
+Route::post('/custom-registration', [CustomAuthController::class, 'customRegistration']) -> name('register.custom'); 
+Route::get('/signout', [CustomAuthController::class, 'signOut']) -> name('signout');
 Route::get('/livros/novo', [LivrosController::class, 'create']);
 Route::post('/livros/novo', [LivrosController::class, 'store']) -> name('registrar_livro');
 Route::get('/livro/ver/{id}', [LivrosController::class, 'show']);
 Route::get('/livro/editar/{id}', [LivrosController::class, 'edit']);
 Route::post('/livro/editar/{id}', [LivrosController::class, 'update']) -> name('alterar_livro');
 Route::get('/livro/excluir/{id}', [LivrosController::class,  'delete']);
-Route::post('livro/excluir/{id}', [LivrosController::class, 'destroy']) -> name('excluir_livro');
+Route::post('/livro/excluir/{id}', [LivrosController::class, 'destroy']) -> name('excluir_livro');
